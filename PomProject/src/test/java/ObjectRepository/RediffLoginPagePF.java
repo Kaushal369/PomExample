06c35b527d72834cb312baa10d1e0bef2429a89f
kaushal369@gmail.com <kaushal369@gmail.com>
2019-02-06 01:57:34 +0530

    testing2.

--- PomProject/src/test/java/ObjectRepository/RediffLoginPagePF.java
+++ PomProject/src/test/java/ObjectRepository/RediffLoginPagePF.java
@@ -36,7 +36,7 @@ public class RediffLoginPagePF {
 	
 	@FindBy(xpath=".//div[@class='head-wrapper']/div[2]/a")
 
-	WebElement backToHome;  // updated by kaushal joshi.....
+	WebElement backToHome;  // updated by kaushal joshi.
 
 	
 	public WebElement signinLink()
